03871d5595479e3f45b268e93b058d6cfe4b778d
Bonface <bonniewanga34@gmail.com>
2023-10-10 14:17:36 +0300

    Completed assignments

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str):
+            self._brand = value
+        else:
+            print("Brand must be a string.")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("Size must be an integer.")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
